0fb255337a8ee28ad73814bb3818cb09b1a35964
asif-nawaz01 <asifnawaz51111@gmail.com>
2025-06-13 22:29:14 +0530

    Server

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -36,6 +36,11 @@ const url = require('url');
 ////////////////////////////////////////////////////////
 //SERVER
 
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+ const dataObj = JSON.parse(data);
+
+
 const server = http.createServer((req, res) => {
    const pathName = req.url;
 
@@ -43,7 +48,17 @@ const server = http.createServer((req, res) => {
        res.end('This is the OVERVIEW');
    }else if (pathName === '/product') {
        res.end('This is the PRODUCT');
-   }else {
+   }else if (pathName === '/api') {
+
+fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
+    const productData = JSON.parse(data);
+    console.log(productData);
+    res.writeHead(200, { 'Content-type': 'application/json' });
+    res.end(data);
+});
+    
+
+   }else{
        res.writeHead(404, {
         'Content-type': 'text/html',
         'my-own-header': 'hello-world'
