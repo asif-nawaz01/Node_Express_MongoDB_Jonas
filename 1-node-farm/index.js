ee28ad20358e4357fafbda61c401dee2e9f82f8a
asif-nawaz01 <asifnawaz51111@gmail.com>
2025-06-13 20:35:04 +0530

    Server

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,4 +1,11 @@
 const fs = require('fs');
+const http = require('http');
+const path = require('path');
+const url = require('url');
+
+
+//////////////////////////////////////////////
+//FILES
 
 //Blocking synchronous way 
 
@@ -10,21 +17,42 @@ const fs = require('fs');
 
 //Non-blocking asynchronous way
 
-fs.readFile('./txt/start.txt', 'utf-8', (err,data1) =>{
-    if (err) return console.log('Error 💀');
+// fs.readFile('./txt/start.txt', 'utf-8', (err,data1) =>{
+//     if (err) return console.log('Error 💀');
 
-  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err,data2) =>{
-    console.log(data2);
-    fs.readFile(`./txt/append.txt`, 'utf-8', (err,data3) =>{
-        console.log(data3);
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err,data2) =>{
+//     console.log(data2);
+//     fs.readFile(`./txt/append.txt`, 'utf-8', (err,data3) =>{
+//         console.log(data3);
 
-        fs.writeFile(`./txt/final.txt`, `${data2}\n${data3}`, 'utf-8', err =>{
-            console.log('Your file has been written 😄');
-        });
-    });
-  });
+//         fs.writeFile(`./txt/final.txt`, `${data2}\n${data3}`, 'utf-8', err =>{
+//             console.log('Your file has been written 😄');
+//         });
+//     });
+//   });
+// });
+//  console.log('Will read file!');
+
+////////////////////////////////////////////////////////
+//SERVER
+
+const server = http.createServer((req, res) => {
+   const pathName = req.url;
+
+   if (pathName === '/' || pathName === '/overview') {
+       res.end('This is the OVERVIEW');
+   }else if (pathName === '/product') {
+       res.end('This is the PRODUCT');
+   }else {
+       res.writeHead(404, {
+        'Content-type': 'text/html',
+        'my-own-header': 'hello-world'
+       });
+         res.end('<h1>Page not found</h1>');
+    }
 });
- console.log('Will read file!');
 
-
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to requests on port 8000');
+});
 
